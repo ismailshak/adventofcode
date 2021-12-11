e517db6f9ee71cfe65c2159f1f05ba2d678332af
ismailshak <ismailshak94@yahoo.com>
2021-12-11 15:51:11 -0500

    Day 11

---
+++ 2021/day11/main.go
@@ -0,0 +1,151 @@
+// Advent of Code Day 11: Dumbo Octopus
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	fmt.Println("\nSolving Dumbo Octopus")
+	fmt.Println("---------------------")
+	fmt.Println()
+
+	// Part 1
+	p1Start := time.Now()
+	p1Result := determineFlashes(100)
+	p1Duration := time.Since(p1Start)
+	fmt.Printf("Part 1 Result: %v (%v)\n", p1Result, p1Duration)
+
+	// Part 2
+	p2Start := time.Now()
+	p2Result := findStepWithMaxFlashes(1000) // idk some super large number
+	p2Duration := time.Since(p2Start)
+	fmt.Printf("Part 2 Result: %v (%v)\n", p2Result, p2Duration)
+}
+
+func openInputFile() *os.File {
+	inputFile, err := os.Open("./input.txt")
+	if err != nil {
+		panic("Error. Failed to read input file.")
+	}
+	return inputFile
+}
+
+func parseInt(value string) int {
+	output, err := strconv.ParseInt(value, 10, 32)
+	if err != nil {
+		panic(err)
+	}
+	return int(output)
+}
+
+func printGrid(grid *[10][10]int) {
+	for _, row := range grid {
+		for _, col := range row {
+			fmt.Printf(" %3d", col)
+		}
+		fmt.Print("\n")
+	}
+	fmt.Print("---------\n")
+}
+
+func bumpEnergyLevels(grid *[10][10]int) {
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			grid[i][j]++
+		}
+	}
+}
+
+// terrible.
+func simulateFlashes(grid *[10][10]int) int {
+	flashCount := 0
+	didIterationFlash := true
+
+	for didIterationFlash {
+		didIterationFlash = false
+		for i := 0; i < 10; i++ {
+			for j := 0; j < 10; j++ {
+				if grid[i][j] > 9 {
+					grid[i][j] = 0
+					//printGrid(grid)
+					flashCount++
+					didIterationFlash = true
+					up := (i - 1) >= 0
+					down := (i + 1) < 10
+					left := (j - 1) >= 0
+					right := (j + 1) < 10
+
+					if up && grid[i-1][j] != 0 {
+						grid[i-1][j] += 1
+					}
+					if down && grid[i+1][j] != 0 {
+						grid[i+1][j] += 1
+					}
+					if left && grid[i][j-1] != 0 {
+						grid[i][j-1] += 1
+					}
+					if right && grid[i][j+1] != 0 {
+						grid[i][j+1] += 1
+					}
+					if up && left && grid[i-1][j-1] != 0 {
+						grid[i-1][j-1] += 1
+					}
+					if up && right && grid[i-1][j+1] != 0 {
+						grid[i-1][j+1] += 1
+					}
+					if down && left && grid[i+1][j-1] != 0 {
+						grid[i+1][j-1] += 1
+					}
+					if down && right && grid[i+1][j+1] != 0 {
+						grid[i+1][j+1] += 1
+					}
+				}
+			}
+		}
+	}
+
+	return flashCount
+}
+
+func determineFlashes(steps int) int {
+	inputFile := openInputFile()
+	defer inputFile.Close()
+
+	input := bufio.NewScanner(inputFile)
+	grid := [10][10]int{}
+
+	// build grid
+	for i := 0; i < 10; i++ {
+		input.Scan()
+		line := strings.Split(input.Text(), "")
+		for j := 0; j < 10; j++ {
+			grid[i][j] = parseInt(line[j])
+		}
+	}
+
+	// simulate evolution
+	totalFlashes := 0
+	for k := 0; k < steps; k++ {
+		bumpEnergyLevels(&grid)
+		flashCount := simulateFlashes(&grid)
+		// for part 2, I'll pass a super large steps value so that when
+		// all octopi flash the return will be the iteration (vs the flash count :shrug:)
+		if flashCount == 100 {
+			return k
+		}
+
+		totalFlashes += flashCount
+	}
+
+	return totalFlashes
+}
+
+func findStepWithMaxFlashes(steps int) int {
+	return determineFlashes(steps) + 1 // adding 1 because part 1's function started at step 0
+}
