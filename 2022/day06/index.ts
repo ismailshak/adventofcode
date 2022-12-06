f46e9f6d7d6332dde007c2825760584333cb4bda
ismailshak <ismailshak94@yahoo.com>
2022-12-06 07:45:50 -0500

    Day 6 pt.2

--- 2022/day06/index.ts
+++ 2022/day06/index.ts
@@ -1,20 +1,21 @@
-import {inputToLines, parseInput} from "@utils/input";
+import {parseInput} from "@utils/input";
 import {run} from "@utils/runner";
 
 const input = parseInput(__dirname);
 
-const arePreviousFourUnique = (index: number) => {
-  const substr = input.slice(index - 4, index);
-  return new Set(substr.split("")).size === 4;
+const arePreviousXUnique = (index: number, count: number) => {
+  const substr = input.slice(index - count, index);
+  return new Set(substr.split("")).size === count;
 };
 
 const part1 = () => {
+  const uniqueCount = 4;
   for (let i = 0; i < input.length; i++) {
-    if (i < 4) {
+    if (i < uniqueCount) {
       continue;
     }
 
-    if (arePreviousFourUnique(i)) {
+    if (arePreviousXUnique(i, uniqueCount)) {
       return i;
     }
   }
@@ -22,10 +23,23 @@ const part1 = () => {
   return 0;
 };
 
-const part2 = () => 0;
+const part2 = () => {
+  const uniqueCount = 14;
+  for (let i = 0; i < input.length; i++) {
+    if (i < uniqueCount) {
+      continue;
+    }
+
+    if (arePreviousXUnique(i, uniqueCount)) {
+      return i;
+    }
+  }
+
+  return 0;
+};
 
 run(
   {day: 6, title: "Tuning Trouble"},
-  {solution: part1, message: "placeholder"},
-  {solution: part2, message: "placeholder"}
+  {solution: part1, message: "Characters processed before 4 unique found"},
+  {solution: part2, message: "Characters processed before 14 unique found"}
 );
