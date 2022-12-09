d3052ceecf8793f942b5ccd3b84dd6d4e3393602
Ismail Elshakankiry <ismailshak94@yahoo.com>
2022-12-09 10:43:54 -0500

    Day 8 pt.2

--- 2022/day08/index.ts
+++ 2022/day08/index.ts
@@ -8,12 +8,12 @@ interface Args {
   charIndex: number;
 }
 
-const isTreeVisible = (currentHeight: number, treeHeight: string) => currentHeight <= parseInt(treeHeight, 10);
+const isTreeBlocked = (currentHeight: number, treeHeight: string) => currentHeight <= parseInt(treeHeight, 10);
 
 const isTopVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
   const currentHeight = parseInt(currentLine[charIndex], 10);
   for (let i = lineIndex - 1; i >= 0; i--) {
-    if (isTreeVisible(currentHeight, lines[i][charIndex])) {
+    if (isTreeBlocked(currentHeight, lines[i][charIndex])) {
       return false;
     }
   }
@@ -24,7 +24,7 @@ const isTopVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
 const isBottomVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
   const currentHeight = parseInt(currentLine[charIndex], 10);
   for (let i = lineIndex + 1; i < lines.length; i++) {
-    if (isTreeVisible(currentHeight, lines[i][charIndex])) {
+    if (isTreeBlocked(currentHeight, lines[i][charIndex])) {
       return false;
     }
   }
@@ -35,7 +35,7 @@ const isBottomVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
 const isLeftVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
   const currentHeight = parseInt(currentLine[charIndex], 10);
   for (let i = charIndex - 1; i >= 0; i--) {
-    if (isTreeVisible(currentHeight, lines[lineIndex][i])) {
+    if (isTreeBlocked(currentHeight, lines[lineIndex][i])) {
       return false;
     }
   }
@@ -46,7 +46,7 @@ const isLeftVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
 const isRightVisible = ({lines, currentLine, lineIndex, charIndex}: Args) => {
   const currentHeight = parseInt(currentLine[charIndex], 10);
   for (let i = charIndex + 1; i < currentLine.length; i++) {
-    if (isTreeVisible(currentHeight, lines[lineIndex][i])) {
+    if (isTreeBlocked(currentHeight, lines[lineIndex][i])) {
       return false;
     }
   }
@@ -79,8 +79,6 @@ const part1 = (input: string) => {
 
   const defaultVisible = countPerimeterTrees(lines);
 
-  console.log(defaultVisible);
-
   const innerVisibleCount = lines.reduce((acc, line, index) => {
     if (index === 0 || index === lines.length - 1) return acc;
 
@@ -90,10 +88,94 @@ const part1 = (input: string) => {
   return defaultVisible + innerVisibleCount;
 };
 
-const part2 = (input: string) => 0;
+const topScore = ({lines, currentLine, lineIndex, charIndex}: Args) => {
+  if (lineIndex === 0) return 0;
+
+  const currentHeight = parseInt(currentLine[charIndex], 10);
+  const firstIndex = lineIndex - 1;
+  for (let i = firstIndex; i >= 0; i--) {
+    if (isTreeBlocked(currentHeight, lines[i][charIndex])) {
+      return firstIndex - i + 1;
+    }
+  }
+
+  return lineIndex;
+};
+
+const bottomScore = ({lines, currentLine, lineIndex, charIndex}: Args) => {
+  if (lineIndex === lines.length - 1) return 0;
+
+  const currentHeight = parseInt(currentLine[charIndex], 10);
+  const firstIndex = lineIndex + 1;
+  for (let i = firstIndex; i < lines.length; i++) {
+    if (isTreeBlocked(currentHeight, lines[i][charIndex])) {
+      return i - firstIndex + 1;
+    }
+  }
+
+  return lines.length - 1 - lineIndex;
+};
+
+const leftScore = ({lines, currentLine, lineIndex, charIndex}: Args) => {
+  if (charIndex === 0) return 0;
+
+  const currentHeight = parseInt(currentLine[charIndex], 10);
+  const firstIndex = charIndex - 1;
+  for (let i = firstIndex; i >= 0; i--) {
+    if (isTreeBlocked(currentHeight, lines[lineIndex][i])) {
+      return firstIndex - i + 1;
+    }
+  }
+
+  return charIndex;
+};
+
+const rightScore = ({lines, currentLine, lineIndex, charIndex}: Args) => {
+  if (charIndex === currentLine.length - 1) return 0;
+
+  const currentHeight = parseInt(currentLine[charIndex], 10);
+  const firstIndex = charIndex + 1;
+  for (let i = charIndex + 1; i < currentLine.length; i++) {
+    if (isTreeBlocked(currentHeight, lines[lineIndex][i])) {
+      return i - firstIndex + 1;
+    }
+  }
+
+  return currentLine.length - 1 - charIndex;
+};
+
+const calculateScenicScore = (args: Args) => topScore(args) * bottomScore(args) * leftScore(args) * rightScore(args);
+
+const findBestScoreInRow = (lines: Array<string>, currentLine: string, lineIndex: number) => {
+  let highestScore = 0;
+
+  for (let i = 1; i < currentLine.length; i++) {
+    const currentTreeScore = calculateScenicScore({lines, currentLine, lineIndex, charIndex: i});
+    if (currentTreeScore > highestScore) {
+      highestScore = currentTreeScore;
+    }
+  }
+
+  return highestScore;
+};
+
+const part2 = (input: string) => {
+  const lines = inputToLines(input);
+
+  const bestScenicScore = lines.reduce((acc, line, index) => {
+    const bestScore = findBestScoreInRow(lines, line, index);
+    if (bestScore > acc) {
+      return bestScore;
+    }
+
+    return acc;
+  }, 0);
+
+  return bestScenicScore;
+};
 
 run(
-  {cwd: __dirname, day: 8, title: "Treetop Tree House"},
-  {solution: part1, message: "Number of trees visible from the outside grid"},
-  {solution: part2, message: "placeholder"}
+  {cwd: __dirname, day: 8, title: "Treetop Tree House", mock: false},
+  {solution: part1, message: "Number of trees visible from outside the grid"},
+  {solution: part2, message: "Highest scenic score"}
 );
