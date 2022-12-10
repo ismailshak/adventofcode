5bf8926723609356d2532b6c45efce420d4c9295
ismailshak <ismailshak94@yahoo.com>
2022-12-10 11:28:43 -0500

    Day 9 pt 2

--- 2022/day09/index.ts
+++ 2022/day09/index.ts
@@ -1,6 +1,8 @@
 import {inputToLines} from "@utils/input";
 import {run} from "@utils/runner";
 
+type Coords = {x: number; y: number};
+
 enum Direction {
   UP = "U",
   DOWN = "D",
@@ -12,6 +14,52 @@ interface Move {
   direction: Direction;
   count: number;
 }
+
+const moveHead = (direction: Direction, head: Coords) => {
+  switch (direction) {
+    case Direction.UP:
+      head.y++;
+      break;
+    case Direction.DOWN:
+      head.y--;
+      break;
+    case Direction.LEFT:
+      head.x--;
+      break;
+    case Direction.RIGHT:
+      head.x++;
+      break;
+    default:
+      throw `${direction} did not find a matching switch case for head`;
+  }
+
+  return head;
+};
+
+const moveTail = (head: Coords, tail: Coords) => {
+  const diffX = Math.abs(head.x - tail.x);
+  const diffY = Math.abs(head.y - tail.y);
+
+  if (diffX < 2 && diffY < 2) {
+    return {current: tail, parent: head};
+  }
+
+  if (diffX > 1 && !diffY) {
+    tail.x += head.x - tail.x > 0 ? 1 : -1;
+  } else if (diffY > 1 && !diffX) {
+    tail.y += head.y - tail.y > 0 ? 1 : -1;
+  } else {
+    tail.x += head.x - tail.x > 0 ? 1 : -1;
+    tail.y += head.y - tail.y > 0 ? 1 : -1;
+  }
+
+  return {current: tail, parent: head};
+};
+
+const moveKnot = (direction: Direction, parent: Coords, current: Coords) => {
+  const head = moveHead(direction, parent);
+  return moveTail(head, current);
+};
 
 class FakeGrid {
   private set: Set<string>;
@@ -34,38 +82,12 @@ class FakeGrid {
   }
 
   move(direction: Direction) {
-    switch (direction) {
-      case Direction.UP:
-        this.hY++;
-        if (Math.abs(this.tY - this.hY) > 1) {
-          this.tX = this.hX;
-          this.tY = this.hY - 1;
-        }
-        break;
-      case Direction.DOWN:
-        this.hY--;
-        if (Math.abs(this.tY - this.hY) > 1) {
-          this.tX = this.hX;
-          this.tY = this.hY + 1;
-        }
-        break;
-      case Direction.LEFT:
-        this.hX--;
-        if (Math.abs(this.tX - this.hX) > 1) {
-          this.tY = this.hY;
-          this.tX = this.hX + 1;
-        }
-        break;
-      case Direction.RIGHT:
-        this.hX++;
-        if (Math.abs(this.tX - this.hX) > 1) {
-          this.tY = this.hY;
-          this.tX = this.hX - 1;
-        }
-        break;
-      default:
-        throw `${direction} did not find a matching switch case`;
-    }
+    const {current, parent} = moveKnot(direction, {x: this.hX, y: this.hY}, {x: this.tX, y: this.tY});
+
+    this.hX = parent.x;
+    this.hY = parent.y;
+    this.tX = current.x;
+    this.tY = current.y;
 
     this.markVisited();
   }
@@ -109,7 +131,38 @@ const part1 = (input: string) => {
   return fakeGrid.getVisitedCount();
 };
 
-const part2 = (input: string) => 0;
+const moveRope = (move: Move, rope: Array<Coords>, set: Set<string>) => {
+  for (let i = 0; i < move.count; i++) {
+    rope.forEach((knot, index) => {
+      if (index === 0) {
+        const head = moveHead(move.direction, knot);
+        rope[0] = head;
+        return;
+      }
+
+      const {current} = moveTail(rope[index - 1], knot);
+      if (index === rope.length - 1) {
+        set.add(`${current.x},${current.y}`);
+      }
+    });
+  }
+};
+
+const part2 = (input: string) => {
+  const ropeSize = 10;
+  const lines = inputToLines(input);
+  const tailPositions = new Set<string>();
+  const knots: Array<Coords> = [];
+  for (let i = 0; i < ropeSize; i++) {
+    knots[i] = {x: 11, y: 5};
+  }
+
+  lines.forEach((line) => {
+    const move = parseMove(line);
+    moveRope(move, knots, tailPositions);
+  });
+  return tailPositions.size;
+};
 
 run(
   {cwd: __dirname, day: 9, title: "Rope Bridge", mock: false},
