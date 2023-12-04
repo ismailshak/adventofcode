577f327fd7252ad76206c5850efcbca299922964
ismailshak <ismailshak94@yahoo.com>
2023-12-04 12:33:05 -0500

    Day 4 pt.2

--- 2023/src/day04/mod.rs
+++ 2023/src/day04/mod.rs
@@ -61,11 +61,42 @@ fn part1() -> u32 {
         .sum::<u32>()
 }
 
+fn part2() -> u32 {
+    let winners_regex = Regex::new(r":(.*?)\|").unwrap();
+    let draws_regex = Regex::new(r"\|(.*?)$").unwrap();
+    let mut card_count_map: HashMap<usize, u32> = HashMap::new();
+
+    include_str!("input.txt")
+        .lines()
+        .enumerate()
+        .for_each(|(i, l)| {
+            let current_card = i + 1;
+
+            // Tally the current (original) card
+            let mut current_card_count = *card_count_map.entry(current_card).or_insert(0);
+            card_count_map.insert(current_card, current_card_count + 1);
+            current_card_count += 1; // Keep the copy in sync with the value in the map
+
+            let mut winnings = extract_winning_numbers(&winners_regex, l);
+            let count = find_win_count(&draws_regex, l, &mut winnings);
+            let starting_card = current_card + 1;
+            let ending_card = current_card + count as usize;
+
+            // Add the winnings to the card count map, taking into account the current card's multiplier
+            for n in starting_card..=ending_card {
+                let n_card_count = *card_count_map.entry(n).or_insert(0);
+                card_count_map.insert(n, n_card_count + current_card_count);
+            }
+        });
+
+    card_count_map.values().sum()
+}
+
 pub fn get<'a>() -> Puzzle<'a, u32> {
     Puzzle {
         day: 4,
         title: "Scratchcards",
         part1: PuzzlePart::new("Total winning points", part1),
-        part2: PuzzlePart::new("N/A", || 0),
+        part2: PuzzlePart::new("Total number of cards", part2),
     }
 }
